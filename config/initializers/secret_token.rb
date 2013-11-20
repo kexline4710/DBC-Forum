47886aafca6c5f392359c16efc93da631d3d1dcc
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-20 15:26:30 -0600

    adding rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DBCForum::Application.config.secret_key_base = 'edb0be22fba4034e677793892ecc6e580a50eaffd06f744309f0fbb229ce4f7259c194be33654a7885c4b98e5133507f36e5a797ca911d2f9439ed47f54a3c3e'
