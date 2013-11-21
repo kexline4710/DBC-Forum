261f2e8922edd1fe0f6f224d01785f73783dd92c
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-20 21:59:06 -0600

    added tests for the sessions controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
-describe SessionsController, "#index" do
+describe SessionsController do 
+
+  context "#index" do
 
     context "renders the index template" do
       before do
@@ -10,6 +12,28 @@ describe SessionsController, "#index" do
       it { should respond_with(:success) }
       it { should render_template(:index) }
     end
+  end
 
 
+  context "#new" do
+    context "renders the new template" do
+        before do
+          get :new  
+        end
+
+        it { should respond_with(:success) }
+        it { should render_template(:new) }
+      end
+    end
+
+  context "#create" do
+    before do
+      post :create, :email => "tan@dbc.com", :password => "12234"
+    end
+    
+    context "user login information is incorrect" do
+      it { should redirect_to('/login') }
+    end
+
+  end
 end
