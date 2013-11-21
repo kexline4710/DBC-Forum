75a97f109a8b5b3a21dc0a0488a4a613161c02e8
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-20 20:16:58 -0600

    added login form and made session create route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,22 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def index
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to "/users/#{user.id}"
+    else
+      flash[:notice] = "Email or password incorrect."
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+  end
+
 end
