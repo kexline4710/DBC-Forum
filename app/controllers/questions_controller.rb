10a2353696188ce581553bdac3c2bbaf760e5759
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-21 14:41:34 -0600

    question creation feature and tests

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,7 +4,19 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    render text: params[:question].inspect
+    # render text: params[:question].inspect
+    @question = Question.new(params[:question].merge(asker_id: session[:user_id]) )
+
+    if @question.save
+      redirect_to root_path
+    else
+      redirect_to new_question_path
+    end
   end
 
+  # def question_params
+  #   params.require(:question).permit(:title, :text)
+  # end
+
 end
+
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,11 @@
 class Question < ActiveRecord::Base
   belongs_to :asker, class_name: "User"
   has_many :answers
+
+  attr_accessible :title, :body, :asker_id
+
+  validates :title, :presence => true
+  validates :body, :presence => true
+  validates :asker_id, :presence => true
+
 end
