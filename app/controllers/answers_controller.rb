a782c895b74255193bdb770e42fb028823d597d4
Apprentice <apprentice@dbc-chicago-021.local>
2013-11-22 15:23:41 -0600

    fixed current_user helper method

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,6 @@
 class AnswersController < ApplicationController
   def create
-    answer = Answer.create!(body: params[:answer][:body], question_id: params[:question_id], responder_id: session[:user_id])
+    answer = Answer.create!(body: params[:answer][:body], question_id: params[:question_id], responder_id: current_user.id  )
     redirect_to question_path(params[:question_id])
   end
 end
