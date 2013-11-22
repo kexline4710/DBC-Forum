29a46c965373b0a903f9fa45b7ddea35c8bda7f3
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-22 17:16:21 -0600

    need to pull in feature corrections

--- spec/features/user_visits_profile_spec.rb
+++ spec/features/user_visits_profile_spec.rb
@@ -1,10 +1,14 @@
 require 'spec_helper'
 
-# feature "the user profile page" do
-#   let(:user) { FactoryGirl.create(:user) }
+feature "the user profile page" do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:question) { FactoryGirl.create(:question) }
+   
+  
 
-#   scenario "should display user's questions" do
-#     visit user_path(user)
-#     expect(page).to have_content user.questions
-#   end
-# end
+  scenario "should display user's questions" do
+    user.questions << question
+    visit user_path(user)
+    expect(page).to have_link(question.title)
+  end
+end
