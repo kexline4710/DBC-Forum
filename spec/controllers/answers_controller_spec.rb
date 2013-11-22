5d0617da5a0e99ea9545ee9f64492a62219590bb
Apprentice <apprentice@dbc-chicago-021.local>
2013-11-22 15:40:19 -0600

    fiexed answers create tests

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -3,13 +3,18 @@ require 'spec_helper'
 describe AnswersController do
 
   context "#create" do
+    before(:all) do
+      @user = User.create(name: "Oliver", email: "a@a.com", password: "Password", password_confirmation: "Password")
+    end
     it "creates the answers object" do
-      get :create, :answer => {body: "body"}, question_id: 1, user_id: 1
+      session[:user_id] = @user.id
+      get :create, :answer => {body: "body"}, question_id: 1, responder_id: 1
       Answer.last.body.should eq("body")
     end
 
     it "redirects to the question page" do
-      get :create, :answer => {body: "test body"}, question_id: 1, user_id: 1
+      session[:user_id] = @user.id
+      get :create, :answer => {body: "test body"}, question_id: 1, responder_id: 1
       response.should redirect_to question_path(1)
     end
   end
