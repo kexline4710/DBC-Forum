ca60b5bf8b9bede566c020a8eaa7281457619343
kexline4710 <kathryn.exline@gmail.com>
2013-11-21 18:40:32 -0600

    configuring for heroku

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ Bundler.require(:default, Rails.env)
 
 module DBCForum
   class Application < Rails::Application
+    # config.assets.version = '1.0'
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
