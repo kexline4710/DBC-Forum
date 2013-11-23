04cb6ebaf1bcdba40b6851b45568d1b84a1daac3
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-22 18:48:03 -0600

    put coverage folder into .gitignore

--- spec/features/user_visits_profile_spec.rb
+++ spec/features/user_visits_profile_spec.rb
@@ -5,6 +5,7 @@ feature "the user profile page" do
   let(:question) { FactoryGirl.create(:question, :asker => user) }
    
   scenario "should display user's questions" do
+  
     question
 
     # visit new user page
@@ -12,7 +13,8 @@ feature "the user profile page" do
     # visit user's profile
     # click create question
     # create question
-    
+
+
     visit user_path(user)
 
     expect(page).to have_link(question.title)
