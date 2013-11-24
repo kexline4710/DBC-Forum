3638516b6ff1499c0f4c95fdda4aca77ee231f25
kexline4710 <kathryn.exline@gmail.com>
2013-11-24 13:34:05 -0600

    up and downvotes are now seen

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -7,7 +7,15 @@ class Answer < ActiveRecord::Base
 
   def vote_score
   	@upvotes = self.votes.where("state = ?", 1).count 		# 3
-  	@downvotes = self.votes.where("state = ?", -1).count	# 1
+  	@downvotes = self.votes.where("state = ?", 0).count	# 1
   	vote_score = @upvotes - @downvotes
+  end
+
+  def upvotes_count
+  	@upvotes = self.votes.where("state = ?", 1).count
+  end
+
+  def downvotes_count
+  	@downvotes = self.votes.where("state = ?", 0).count
   end
 end
