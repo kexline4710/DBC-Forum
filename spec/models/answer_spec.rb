cebf551c6e95013d60cf24802ccadc857905d558
kexline4710 <kathryn.exline@gmail.com>
2013-11-23 20:36:11 -0600

    added a test for vote_score

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -6,6 +6,10 @@ describe Answer do
   it { should belong_to(:responder) }
   it { should have_many(:votes) }
 
-  #need to test vote_score
+  it "returns a vote_score" do
+		answer = FactoryGirl.create(:answer)
+  	answer.votes.create(voter_id: 1, state: 1)
+  	answer.vote_score.should == 1
+  end
 
 end
