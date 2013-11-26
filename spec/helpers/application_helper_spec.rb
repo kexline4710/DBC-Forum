458fc1b1798a829e152c22abba71b39e5b6ee36e
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-25 19:28:09 -0600

    added specs for the flash_class method in application helper

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,11 +2,21 @@ require 'spec_helper'
 
 describe ApplicationHelper do
 
-	describe "#current_user" do
+	context "#current_user" do
 		it "returns the current user of the session" do
 			user = FactoryGirl.create(:user)
 			session[:user_id] = user.id
 			helper.current_user.should eql(User.find(user.id))
 		end
 	end
+
+    context "#flash_class" do
+      it "returns the correct flash class if success"  do
+        flash_class(:success).should eq "success"
+      end
+
+      it "returns the correct flash class if an error" do
+        flash_class(:error).should eq "error"
+      end
+    end
 end
